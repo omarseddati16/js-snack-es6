7af5aa66bb7f56a0fe52abd4362c3201da738cf4
omarseddati16 <omarseddati16@gmail.com>
2025-07-14 13:53:42 +0200

    snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 // Snack 1
+
+const bicycle = [
+  {
+    nome: "rossa",
+    peso: "7,2",
+  },
+  {
+    nome: "blu",
+    peso: "7,0",
+  },
+  {
+    nome: "verde",
+    peso: "7,1",
+  },
+  {
+    nome: "bianca",
+    peso: "6,9",
+  },
+  {
+    nome: "nera",
+    peso: "7,5",
+  },
+]
+
+let lightweightBicycles = bicycle[0]
+
+for (let i = 1; i < bicycle.length; i++) {
+  const currentbicycle = bicycle[i]
+  if (currentbicycle.peso < lightweightBicycles.peso)
+    lightweightBicycles = currentbicycle
+}
+
+console.log(lightweightBicycles)
